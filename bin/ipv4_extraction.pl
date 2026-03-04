5d5d7159182f26e8bb5445add593bebafea3fe69
mi-ctr1 <252819888+mi-ctr1@users.noreply.github.com>
2026-03-04 17:56:05 -0500

    Add IPv4 extraction script in Perl

--- bin/ipv4_extraction.pl
+++ bin/ipv4_extraction.pl
@@ -1,1 +1,13 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
 
+my $file = 'sample_logs/app.log';
+open my $fh, '<', $file or die $!;
+
+while (my $line = <$fh>) {
+  while ($line =~ /\b(\d{1,3}(?:\.\d{1,3}){3})\b/g) {
+    print "$1\n";
+  }
+}
+close $fh;
